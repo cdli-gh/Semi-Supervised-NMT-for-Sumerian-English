9392aa440c1dbfa6159644d65cdc27d6e3f9ec8d
Rachit Bansal <rachitbansal2500@gmail.com>
2020-07-02 20:27:11 +0530

    Update btPipeline.sh

--- translation/backtranslation-onmt/btPipeline.sh
+++ translation/backtranslation-onmt/btPipeline.sh
@@ -1,18 +1,25 @@
 numShards=9
+weightsDir=${1:-../../weights/BT}
+shardSrcDir=${2:-./src}
+shardBackDir=${3:-./backT}
+evalSrcDir=${4:-../../dataset/dataToUse/UrIIICompSents}
+evalTgtDir=${5:-./}
+
 for shard in 0 1 2 3 4 5 6 7 8
 do
   val=`expr $shard + 1`
-  if [ -f ../../weights/BT/${val}st ]; then
+  echo $val
+  if [ -d "${weightsDir}/${val}st/" ]; then
       echo "found existing, skipping"
   else
-    echo "############################ Back Translating Sumerian using previous weights ############################"
-    python3 backtranslateONMT.py --model ../../weights/BT/${shard}st/weights_step_10000.pt
-    echo "############################ Stacking ############################"
-    python3 stack.py --backSrc ./src/shard${shard}.sum ./backT/shard${shard}.eng
-    mkdir ../../weights/BT/${val}st/
-    echo "############################ Re-Training ############################"
-    sh runTransformerSumEn.sh ./backed ../../weights/BT/${val}st/
-    echo "############################ Evaluating ############################"
-    onmt_translate -model ../../weights/BT/${val}st/weights_step_10000.pt -src ../../../dataset/dataToUse/UrIIICompSents/evaluate.sum -output ./predd_eng${val}.txt -replace_unk -verbose
+    echo "########### Back Translating Sumerian using previous weights ############"
+    python3 backtranslateONMT.py --srcFile shard${shard}.sum --model ${weightsDir}/${shard}st/_step_10000.pt
+    echo "########### Stacking ############"
+    python3 stack.py --backSrc ${shardSrcDir}/shard${shard}.sum --backTgt ${shardBackDir}/shard${shard}.eng
+    mkdir ${weightsDir}/${val}st/
+    echo "########### Re-Training ###########"
+    sh runTransformerSumEn.sh ./backed ${weightsDir}/${val}st/
+    echo "########### Evaluating #############"
+    onmt_translate -model ${weightsDir}/${val}st/_step_10000.pt -src ${evalSrcDir}/evaluate.sum -output ${evalTgtDir}/predd_eng${val}.txt -replace_unk -verbose
   fi
 done
