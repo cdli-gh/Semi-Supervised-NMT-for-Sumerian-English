573e50cac7e0c50a9e0b9f22b9ebd2382f177adc
Rachit Bansal <rachitbansal2500@hmail.com>
2020-07-02 20:41:52 +0530

    updated mixing.py

--- helpers/mixing.py
+++ helpers/mixing.py
@@ -1,34 +1,47 @@
 import os
 import numpy as np
 
-types = ["train", "test", "dev"]
+# types = ["train", "test", "dev"]
 
-savedir = '../dataset/dataToUse/allCompSents/'
-urdir = '../dataset/dataToUse/UrIIICompSents/'
+# savedir = '../dataset/dataToUse/allCompSents/'
+# urdir = '../dataset/dataToUse/UrIIICompSents/'
 
-def savefile(filename,LIST):
-    with open(savedir + filename, 'a') as f:
-        for line in LIST:
-            f.write("%s" % line)
+# def savefile(filename,LIST):
+#     with open(savedir + filename, 'a') as f:
+#         for line in LIST:
+#             f.write("%s" % line)
 
-for filee in os.listdir(urdir):
-    for t in types:
-        lines_s = ""
-        lines_e = ""
-        if(filee[:filee.find('.')] == t):
-            with open(urdir + filee, 'r') as f:
-                if filee[-3:] == 'sum':
-                    lines_s += f.read()
-                elif filee[-3:] == 'eng':
-                    lines_e += f.read()
+# for filee in os.listdir(urdir):
+#     for t in types:
+#         lines_s = ""
+#         lines_e = ""
+#         if(filee[:filee.find('.')] == t):
+#             with open(urdir + filee, 'r') as f:
+#                 if filee[-3:] == 'sum':
+#                     lines_s += f.read()
+#                 elif filee[-3:] == 'eng':
+#                     lines_e += f.read()
             
-            savefile(t + '.sum', lines_s)
-            savefile(t + '.eng', lines_e)
+#             savefile(t + '.sum', lines_s)
+#             savefile(t + '.eng', lines_e)
 
 with open('../dataset/dataToUse/UrIIICompSents/test.sum', 'r') as f:
     test_sents = f.read()
 
+with open('../dataset/dataToUse/UrIIICompSents/dev.sum', 'r') as f:
+    dev_sents = f.read()
+
+with open('../dataset/dataToUse/UrIIICompSents/test.eng', 'r') as f:
+    test_sentsPll = f.read()
+
+with open('../dataset/dataToUse/UrIIICompSents/dev.eng', 'r') as f:
+    dev_sentsPll = f.read()
+
 test_sents = test_sents.split('\n')
+test_sents += dev_sents.split('\n')
+
+test_sentsPll = test_sentsPll.split('\n')
+test_sentsPll += dev_sentsPll.split('\n')
 
 with open('../dataset/dataToUse/allCompSents/train.sum', 'r') as f:
     train_sents = f.read()
@@ -40,25 +53,42 @@ train_sents = train_sents.split('\n')
 train_sents2 = train_sents2.split('\n')
 
 print(len(train_sents), len(train_sents2))
+print(len(test_sents), len(test_sentsPll))
+
+toKeep = []
+toKeepPll = []
+
+for i in range(len(test_sents)):
+    if(len(test_sents[i].split()) >= 4 and test_sentsPll[i].find('xxx') == -1 and len(test_sentsPll[i].split()) >= 4):
+        toKeep.append(test_sents[i])
+        toKeepPll.append(test_sentsPll[i])
 
 for i in range(len(train_sents)):
     while(train_sents[i].find('Q') != -1):
         idx = train_sents[i].find('Q')
         train_sents[i] = train_sents[i][0:idx] + train_sents[i][idx+12:]
 
-for i in range(len(train_sents)):
-    try:
-        if train_sents[i] in test_sents or len(train_sents2[i]) == 0 or train_sents2[i] == '':
-            train_sents.pop(i)
-            train_sents2.pop(i)
-            i-=1
-    except:
-        break
+print(len(toKeep), len(toKeepPll))
 
-print(len(train_sents), len(train_sents2))
+with open('../dataset/dataToUse/UrIIICompSents/evaluate.sum', 'w') as f:
+    f.write('\n'.join(toKeep))
 
-with open('../dataset/dataToUse/allCompSents/trainX.eng', 'w') as f:
-    f.write('\n'.join(train_sents2))
+with open('../dataset/dataToUse/UrIIICompSents/evaluate.eng', 'w') as f:
+    f.write('\n'.join(toKeepPll))
 
-with open('../dataset/dataToUse/allCompSents/trainX.sum', 'w') as f:
-    f.write('\n'.join(train_sents))
+# for i in range(len(train_sents)):
+#     try:
+#         if train_sents[i] in test_sents or len(train_sents2[i]) == 0 or train_sents2[i] == '':
+#             train_sents.pop(i)
+#             train_sents2.pop(i)
+#             i-=1
+#     except:
+#         break
+
+# print(len(train_sents), len(train_sents2))
+
+# with open('../dataset/dataToUse/allCompSents/trainX.eng', 'w') as f:
+#     f.write('\n'.join(train_sents2))
+
+# with open('../dataset/dataToUse/allCompSents/trainX.sum', 'w') as f:
+#     f.write('\n'.join(train_sents))
