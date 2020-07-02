0fdb6d7950b7f323361e1043b16b438f10adcfe9
Rachit Bansal <rachitbansal2500@gmail.com>
2020-07-02 20:29:09 +0530

    Update backtranslateONMT.py

--- translation/backtranslation-onmt/backtranslateONMT.py
+++ translation/backtranslation-onmt/backtranslateONMT.py
@@ -3,15 +3,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--srcFile', type=str)
 parser.add_argument('--savedir', type=str, default='./backT')
 parser.add_argument('--model', type=str, default='../../weights/BT/1st/weights_step_13000.pt')
 
 args = parser.parse_args()
 
-saveName = 'shard' + args.model.split('/')[-2][-3] + '.eng'
+saveName = 'shard' + str(int(args.model.split('/')[-2][-3])+1) + '.eng'
 
-for file in os.listdir('./src'):
-  print(file)
-  if(saveName not in os.listdir(savedir)):
-      os.system(f'onmt_translate -model {args.model} -src src/{file} -output {args.savedir}/{saveName} -replace_unk -verbose')
-      break
+print(args.srcFile)
+if(saveName not in os.listdir(args.savedir)):
+    os.system(f'onmt_translate -model {args.model} -src src/{args.srcFile} -output {args.savedir}/{saveName} -replace_unk -verbose')
+else:
+    print('Already Exists')
